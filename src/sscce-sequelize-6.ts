b5ce26f537c86a46bd758cea49db86af49b9d105
Andy Edwards <jedwards@fastmail.com>
2024-07-09 18:05:04 -0500

    feat: initial commit

--- src/sscce-sequelize-6.ts
+++ src/sscce-sequelize-6.ts
@@ -1,10 +1,10 @@
-import { DataTypes, Model } from 'sequelize';
-import { createSequelize6Instance } from '../dev/create-sequelize-instance';
-import { expect } from 'chai';
-import sinon from 'sinon';
+import { DataTypes, Model } from "sequelize";
+import { createSequelize6Instance } from "../dev/create-sequelize-instance";
+import { expect } from "chai";
+import sinon from "sinon";
 
 // if your issue is dialect specific, remove the dialects you don't need to test on.
-export const testingOnDialects = new Set(['mssql', 'sqlite', 'mysql', 'mariadb', 'postgres', 'postgres-native']);
+export const testingOnDialects = new Set(["postgres"]);
 
 // You can delete this file if you don't want your SSCCE to be tested against Sequelize 6
 
@@ -21,14 +21,33 @@ export async function run() {
     },
   });
 
-  class Foo extends Model {}
+  class Foo extends Model {
+    declare _value: any;
+    declare value: any;
+  }
 
-  Foo.init({
-    name: DataTypes.TEXT,
-  }, {
-    sequelize,
-    modelName: 'Foo',
-  });
+  Foo.init(
+    {
+      _value: {
+        type: DataTypes.JSONB,
+        allowNull: false,
+        field: "value",
+      },
+      value: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          return this.getDataValue("_value");
+        },
+        set(value: any) {
+          return this.setDataValue("_value", value);
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Foo",
+    }
+  );
 
   // You can use sinon and chai assertions directly in your SSCCE.
   const spy = sinon.spy();
@@ -36,6 +55,5 @@ export async function run() {
   await sequelize.sync({ force: true });
   expect(spy).to.have.been.called;
 
-  console.log(await Foo.create({ name: 'TS foo' }));
-  expect(await Foo.count()).to.equal(1);
+  await Foo.create({ value: [{ a: 1 }, { b: 2 }] });
 }
